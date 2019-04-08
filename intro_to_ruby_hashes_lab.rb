6ce1763c712a177a510cfdba0e1a8355cb29e055
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-08 19:31:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -53,7 +53,7 @@ def monopoly_with_fourth_tier
   	    :pennsylvania_railroad => {"mortgage_value" => "$200"},
   	    :b_and_o_railroad => {"mortage_value" => "$400"},
   	    
-  	    :shortline => {}
+  	    :shortline => {"mortage_value" => "$800"}
   	  },
   	  :rent_in_dollars => {
   	    one_piece_owned: 25,
